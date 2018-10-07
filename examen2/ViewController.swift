24286bbf53c04e7c8bf9452e13baf63f24f53940
Adrian Blazquez <ablazleon@ono.com>
2018-10-07 10:22:47 +0200

    Initial Commit

---
+++ examen2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  examen2
+//
+//  Created by Adrian on 07/10/2018.
+//  Copyright © 2018 Adrián Blázquez León. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
